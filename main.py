2d0a0c6b6642f6d56c728e5583578439fadf1930
SKParthiv <parthiv.sk.pedapati@gmail.com>
2024-10-07 01:19:33 +0000

    encoder_feedback_def

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from path_planning.rtt_path import rtt_path_planning
 import numpy as np
 import RPi.GPIO as GPIO
 from motors.motors import Motor
+import math
 
 fov = 100 # Edit based on the physical params
 calibration_factor = 12 # Edit based on the physical params
@@ -19,6 +20,10 @@ motor1 = Motor(1, 13, 14, 15, 16, pwm_pin)
 motor2 = Motor(2, 17, 18, 19, 20, pwm_pin)
 motor3 = Motor(3, 21, 22, 23, 24, pwm_pin)
 motor4 = Motor(4, 25, 26, 27, 28, pwm_pin)
+encoder_feedback = [(GPIO.LOW, 0),
+                    (GPIO.LOW, 0),
+                    (GPIO.LOW, 0),
+                    (GPIO.LOW, 0)]
 # Main loop
 while True:
     # Capture image
@@ -31,3 +36,16 @@ while True:
     blocks = define_blocks(masks, calibration_factor , image_width , fov)
 
     # Get encoder feedback
+    positions = []
+    for i in encoder_feedback:
+        positions.append(i[1])
+    encoder_feedback = [motor1.encoder_callback(encoder_feedback[0][0]), 
+                        motor2.encoder_callback(encoder_feedback[1][0]),
+                        motor3.encoder_callback(encoder_feedback[2][0]),
+                        motor4.encoder_callback(encoder_feedback[3][0])]
+    position_differences = []
+    for i in range(4):
+        position_differences.append(encoder_feedback[i][1]-positions[i])
+    
+
+    
