c9ca0821dcfac7eb27497bdd5701e103b03a2c03
SKParthiv <parthiv.sk.pedapati@gmail.com>
2024-10-12 12:38:44 +0000

    calibrate

---
+++ calibrate.py
@@ -0,0 +1,88 @@
+from picamera2 import Picamera2, Preview
+import cv2
+import numpy as np
+import time
+
+# Initialize PiCamera
+image_width = 640
+camera = Picamera2()
+camera_config = camera.create_preview_configuration()
+camera.config(camera_config)
+camera.start_preview(Preview.QTGL)
+camera.start()
+def capture_image():
+
+	# Capture image and save it to the disk
+	camera.capture_file('image.jgp')
+
+	# Load image into OpenCV
+	image = cv2.imread('image.jpg')
+	return image
+	# Capture image and save it to the disk
+	# camera = cv2.VideoCapture(0)
+
+	# ret, frame = camera.read()
+	# if ret:
+	# 	cv2.imshow("img", frame)
+	# 	# Load image into OpenCV
+	# 	image = cv2.imread('/home/pi/image.jpg')
+	# 	return frame
+	# else:
+	# 	return None
+
+def define_blocks(mask, calibration_factor, image_width, fov, colour):
+	# Linear relation based on pre-calibrated data
+	blocks = []
+	color = 0
+	# Find contours in the mask
+	contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	for contour in contours:
+		# Get the bounding box of the contour
+		x, y, w, h = cv2.boundingRect(contour)
+		# Assuming pixel_position is the x-coordinate of the center of the bounding box
+		cv2.drawContours(mask, contour, -1, (0,255,0), 5)
+		cv2.imshow("mask", mask)
+		print(x, w, y, h)
+
+def detect_green(image):
+    # Define RGB color ranges in HSV
+    
+    green_lower = np.array([40, 100, 100])
+    green_upper = np.array([70, 255, 255])
+    
+    # Convert image to HSV
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    green_mask = cv2.inRange(hsv_image, green_lower, green_upper)
+    cv2.imshow(green_mask)
+    return green_mask
+
+def detect_red(image):
+	red_lower = np.array([0, 100, 100])
+	red_upper = np.array([10, 255, 255])
+
+	hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+	red_mask = cv2.inRange(hsv_image, red_lower, red_upper)
+	cv2.imshow(red_mask)
+	return red_mask
+
+def detect_brown(image):
+	brown_lower = np.array([10, 100, 20])
+	brown_upper = np.array([20, 255, 200])
+
+	hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+	brown_mask = cv2.inRange(hsv_image, brown_lower, brown_upper)
+	cv2.imshow(brown_mask)
+	return brown_mask
+
+while True:
+	time.sleep(1)
+	image = capture_image()
+	mask1 = detect_green(image)
+	mask2 = detect_red(image)
+	mask3 = detect_brown(image)
+	define_blocks(mask1)
+	define_blocks(mask2)
+	define_blocks(mask3)
--- mapping/map_builder.py
+++ mapping/map_builder.py
@@ -1,36 +1,48 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle, Circle
-from matplotlib.collections import PatchCollection
-from mapping.blocks import Block
-from mapping.robots import Robot
 
-start_x = 10 # x-cordinate of the bottom left corner of the robot
-start_y = 2.5 # y-coordinate of the bottom left corner of the robot
-length = 5 # Length of the robot (Vertical length)
-width = 5 # Width of the robot (Horizontal lenght)
-Rover = Robot(start_x, start_y, width, length)
+# Arena dimensions
+arena_width = 1171
+arena_height = 1143
 
-# Map Dimensions and co-ordinates of various map feautures
-refuge_x = 100
+# Refuge dimensions and coordinates
+refuge_x = 943  # 1143 - 200 (offset from right side)
 refuge_y = 0
-refuge_width = 5
-refuge_length = 10
-hospital_r = 5
-# Example map of the environment
-def create_map(Rover, blocks):
-    _, ax = plt.subplots()
-    rover_rect = Rectangle((Rover.x, Rover.y), Rover.width, Rover.length, Rover.angle, (Rover.x+(Rover.width/2), Rover.y+(Rover.length/2)))
-    
-    # Draw refuge (rectangular)
-    refuge_rect = Rectangle((refuge_x, refuge_y), refuge_width, refuge_length)
-    ax.add_patch(refuge_rect)
+refuge_width = 200
+refuge_length = 300
 
-    # Draw hospital (semi-circular)
-    hospital_circle = Circle((0, 0), 5)
+# Hospital dimensions
+hospital_center_x = 0
+hospital_center_y = 0
+hospital_radius = 230.5
+
+def create_map():
+    # Create the figure and axis
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, arena_width)
+    ax.set_ylim(0, arena_height)
+    ax.set_aspect('equal')
+
+    # Draw refuge (rectangular)
+    refuge_rect = Rectangle((refuge_x - 100, refuge_y), refuge_width, refuge_length, color="blue", alpha=0.5)
+    elevated = Rectangle((refuge_x, refuge_y), 100, 200, color="green", alpha=0.5)
+    ax.add_patch(refuge_rect)
+    ax.add_patch(elevated)
+
+    # Draw hospital (circular)
+    hospital_circle = Circle((hospital_center_x, hospital_center_y), hospital_radius, color="blue", alpha=0.5)
+    hospital_circle_till_doctor = Circle((hospital_center_x, hospital_center_y), 200, color="green", alpha=0.5)
+    hospital_roof = Circle((hospital_center_x, hospital_center_y), 150.5, color="red", alpha=0.5)
     ax.add_patch(hospital_circle)
-    
-    for block in blocks:
-        block.calculate_coordinates(Rover.x, Rover.y)
-        ax.plot(block.x, block.y, 'ro' if block.color == 'Red' else 'go' if block.color == 'Green' else 'brown', label=block.color)
-    ax.legend()
+    ax.add_patch(hospital_circle_till_doctor)
+    ax.add_patch(hospital_roof)
+
+    # Set titles and labels
+    ax.set_title("Arena Map with Hospital and Refuge")
+    ax.set_xlabel("X-coordinate")
+    ax.set_ylabel("Y-coordinate")
+    ax.legend(["Refuge", "Elevated Area", "Hospital", "Doctor Area", "Hospital Roof"])
+
+    # Show the map
+    plt.grid(True)
     plt.show()
